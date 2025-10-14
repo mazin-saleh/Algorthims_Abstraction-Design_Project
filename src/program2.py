f7d0974356c20c19107e339ffdcf0e82876de7fe
Endry Rodriguez <139926363+endryisnotcool@users.noreply.github.com>
2025-10-13 21:00:35 -0400

    ProblemS2 Solution

--- src/program2.py
+++ src/program2.py
@@ -14,11 +14,51 @@ def program2(n: int, k: int, values: List[int]) -> Tuple[int, List[int]]:
     int:  maximal total value
     List[int]: the indices of the chosen vaults(1-indexed)
     """
-    ############################
-    # Add you code here
-    ############################
+    if n == 0: # edge
+        return 0, []
+    
+    chosenL = [0] * n  # list that indicates the has the maximum value using the vaults given
+    helperL = [-1] * n  # list that will help me keep track if the previous vault chosen is valid considering the next one
+    chosenStatus = [False] * n  # separate list that will keep track of the status of whether we chose the vault or not
 
-    return 0, [1, 2, 3] # replace with your code
+    for vault in range(n):
+        if vault > 0: # best value we can have is the same as the previous vault
+            chosenL[vault] = chosenL[vault - 1]
+            helperL[vault] = helperL[vault - 1]
+        else:  # base cases
+            chosenL[vault] = 0
+            helperL[vault] = -1
+
+        vaultValue = values(vault) #take the value of the current vault we're on
+        if vault - k - 1 >= 0:
+            vaultValue += chosenL[vault - k - 1] # if we can safely pick vault before it, add it to overall value
+
+        # if picking the current vault is better than what we already chose, fix accordingly
+        if vaultValue > chosenL[vault]:
+            chosenL[vault] = vaultValue
+            helperL[vault] = vault - k - 1 # set helper to the last safe vault we could've chosen
+            chosenStatus[vault] = True # set boolean to true to indicate we chose this vault
+        else:
+            chosenStatus[vault] = False
+    
+    finalList = []
+    greatestVault = 0
+
+    # since distribution is unimodal, we can't ensure that the greatest value is at the end so we iterate to find the greatest.
+    for vaults in range(1, n): 
+        if chosenL[vault] > chosenL[greatestVault]:
+            greatestVault = chosenL[greatestVault]
+
+    # iterate through list in reverse order since last vault is 
+    while greatestVault >= 0:
+        if chosenStatus[greatestVault] == True:
+            finalList.append(greatestVault + 1)
+            greatestVault = helperL[greatestVault]
+        else:
+            greatestVault = -1
+
+    finalList.reverse()
+    return chosenL[greatestVault], finalList
 
 
 if __name__ == '__main__':
